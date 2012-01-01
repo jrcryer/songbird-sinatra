a93b4a458c2b2fd4278546a932a5d362fe01394b
James Cryer <j.r.cryer@gmail.com>
2012-01-01 10:36:25 +0000

    Adding base app

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'redis'
+
+redis = Redis.new
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+
+  def random_string(length)
+    rand(36**length).to_s(36)
+  end
+end
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  if params[:url] and not params[:url].empty?
+    @shortcode = random_string 5
+    redis.setnx "links:#{@shortcode}", params[:url]
+  end
+  erb :index
+end
+
+get '/:shortcode' do
+  @url = redis.get "links:#{params[:shortcode]}"
+  redirect @url || '/'
+end
